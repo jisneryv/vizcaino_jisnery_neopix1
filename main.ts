b3e2398ffc48b1b9a3ebe84e3624115ecb5b5e6b
jisneryv <72772612+jisneryv@users.noreply.github.com>
2020-10-20 11:20:39 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+light.setPixelColor(0, light.rgb(255, 0, 0))
+light.setPixelColor(1, light.rgb(255, 255, 0))
+light.setPixelColor(2, light.rgb(0, 255, 0))
+light.setPixelColor(3, light.rgb(0, 255, 150))
+light.setPixelColor(4, light.rgb(0, 255, 255))
+light.setPixelColor(5, light.rgb(0, 0, 120))
+light.setPixelColor(6, light.rgb(0, 0, 255))
